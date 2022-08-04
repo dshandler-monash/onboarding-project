bd2cc9fcb0bee2517895176f2b9fb9e431f31f60
Darren Shandler <dsha0047@student.monash.edu>
2022-08-04 22:38:32 +1000

    added call to weather api

---
+++ website-v1/get_uv.js
@@ -0,0 +1,29 @@
+function httpGet(theUrl) {
+    // http request for a specific url, posts to console and return JSON response
+    var request = new XMLHttpRequest();
+    //Open URL
+    request.open( "GET", theUrl, false ); // false for synchronous request
+    request.send();
+    if (request.status == 200){
+        //If successful response log response and return
+        response = JSON.parse(request.response)
+        console.log(response)
+        return response;
+    } else {
+        //If unsuccessful response log error code and text
+        console.log(`error ${request.status} ${request.statusText}`);
+    }
+}
+
+function get_data(){
+    // placeholder function for specific location
+    var lat = -37.93,
+    lon = 145.12;
+    var url_call = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,daily,alerts&appid=29f32a874ef287325631c36c8d516251';
+    console.log(url_call)
+    response = httpGet(url_call)
+
+    return response
+}
+
+response = get_data()
